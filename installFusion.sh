9bfb161e3da75806e21a3fee4871d2c35c3dcfe8
Julien Allexandre <contact@julien-allexandre.fr>
2025-05-15 16:55:46 +0200

    adding colors

--- installFusion.sh
+++ installFusion.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
+YELLOW='\e[0;33m'
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+RESET='\e[0m'
+
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Ce script doit être exécuté en tant que root. Utilisez 'sudo'."
+    echo -e "${RED}Ce script doit être exécuté en tant que root. Utilisez 'sudo'.${RESET}"
     exit 1
 fi
 
-echo "FusionResolveIT - Installation"
+echo "${GREEN}FusionResolveIT - Installation${RESET}"
 
 function installationDesOutils {
-    echo "Installation des outils nécessaires au bon fonctionnement du script."
+    echo -e "${YELLOW}Installation des outils nécessaires au bon fonctionnement du script.${RESET}"
     apt install jq curl -y
 }
 installationDesOutils
 
 function installationDesPrerequis {
-    echo "Installation des prérequis."
+    echo -e "${YELLOW}Installation des prérequis.${RESET}"
     apt update
     apt install apt-transport-https
     wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg 
     sh -c 'echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
     apt update
-    echo "Installation de Nginx."
+    echo -e "${YELLOW}Installation de Nginx.${RESET}"
     apt install nginx -y
-    echo "Installation de PHP et ses dépendances."
+    echo -e "${YELLOW}Installation de PHP et ses dépendances.${RESET}"
     apt install php$FUSION_PHP_VERSION-{fpm,curl,gd,imap,intl,mbstring,mysql,xml,zip} -y
-    echo "Installation de MariaDB."
+    echo -e "${YELLOW}Installation de MariaDB.${RESET}"
     apt install mariadb-server -y
     configurationBDD
 }
 
 function configurationBDD {
-    echo "Configuration de MariaDB."
+    echo -e "${YELLOW}Configuration de MariaDB.${RESET}"
     mysql -e "CREATE DATABASE IF NOT EXISTS $FUSION_DB_NAME;"
     mysql -e "CREATE USER IF NOT EXISTS $FUSION_DB_USER@$FUSION_DB_HOST IDENTIFIED BY '$FUSION_DB_PASSWORD';"
     mysql -e "GRANT ALL PRIVILEGES ON $FUSION_DB_NAME.* TO $FUSION_DB_USER@$FUSION_DB_HOST;"
@@ -38,14 +43,14 @@ function configurationBDD {
 }
 
 function telechargementFichiersFusion {
-    echo "Téléchargement des fichiers FusionResolveIT."
+    echo -e "${YELLOW}Téléchargement des fichiers FusionResolveIT.${RESET}"
     FUSION_TAR_NAME=$(echo "$FUSION_URL" | jq -r '.assets[0].name')
     if [ -f "/tmp/$FUSION_TAR_NAME" ]; then
         rm -r "/tmp/$FUSION_TAR_NAME"
     fi
     wget -P /tmp $FUSION_DOWNLOAD_URL
     if [ -d "/var/www/fusionresolveit" ]; then
-        echo "Erreur : /var/www/fusionresolveit existe déjà."
+        echo -e "${RED}Erreur : /var/www/fusionresolveit existe déjà.${RESET}"
     else
         tar -xzf "/tmp/$FUSION_TAR_NAME" -C /var/www
     fi
@@ -53,7 +58,7 @@ function telechargementFichiersFusion {
 }
 
 function configurationNginx {
-    echo "Configuration de Nginx."
+    echo -e "${YELLOW}Configuration de Nginx.${RESET}"
     if [ ! -f /etc/nginx/sites-available/fusionresolveit ]; then
     cat > /etc/nginx/sites-available/fusionresolveit << EOF
 server {
@@ -85,13 +90,13 @@ EOF
     rm /etc/nginx/sites-enabled/default
     systemctl restart nginx.service
     else
-    echo "Le fichier /etc/nginx/sites-available/fusionresolveit existe déjà."
+    echo -e "${RED}Le fichier /etc/nginx/sites-available/fusionresolveit existe déjà.${RESET}"
     fi
     configurationPhinx
 }
 
 function configurationPhinx {
-    echo "Edition du fichier Phinx."
+    echo -e "${YELLOW}Edition du fichier Phinx.${RESET}"
     if [ ! -f /var/www/fusionresolveit/phinx.php ]; then
     cat > /var/www/fusionresolveit/phinx.php << EOF
 <?php
@@ -118,28 +123,28 @@ return [
 ];
 EOF
     else
-    echo "Le fichier /var/www/fusionresolveit/phinx.php existe déjà."
+    echo -e "${RED}Le fichier /var/www/fusionresolveit/phinx.php existe déjà.${RESET}"
     fi
     permissionsFichiers
 }
 
 function permissionsFichiers {
-    echo "Définition des permissions."
+    echo -e "${YELLOW}Définition des permissions.${RESET}"
     chown -R www-data:www-data /var/www/fusionresolveit
     chmod -R 755 /var/www/fusionresolveit
     startMigration
 }
 
 function startMigration {
-    echo "Migration de la BDD."
+    echo -e "${YELLOW}Migration de la BDD${RESET}."
     cd /var/www/fusionresolveit
     ./bin/cli migrate
     finish
 }
 
 function finish {
-    echo "Tout est prêt, rendez-vous sur http://IPDELAMACHINE !"
-    echo "Les identifiants par défaut sont admin / adminIT."
+    echo -e "${GREEN}Tout est prêt, rendez-vous sur http://IPDELAMACHINE !${RESET}"
+    echo -e "${GREEN}Les identifiants par défaut sont admin / adminIT.${RESET}"
 }
 
 PS3="Quelle version de PHP souhaitez-vous utiliser ? "
@@ -155,7 +160,7 @@ select choix in "${options[@]}"; do
             break
             ;;
         *)
-            echo "Version invalide"
+            echo -e "${RED}Version invalide${RESET}"
             ;;
     esac
 done
@@ -174,13 +179,13 @@ echo "Quel mot de passe souhaitez-vous utilisé pour la base de données ? "
 read -s FUSION_DB_PASSWORD
 
 echo " "
-echo "Voici un récapitulatif :"
+echo -e "${GREEN}Voici un récapitulatif ${RESET}:"
 echo " "
-echo "PHP Version: $FUSION_PHP_VERSION"
-echo "FusionResolveIT Version: $FUSION_VERSION ($FUSION_DOWNLOAD_URL)"
-echo "Hôte BDD : $FUSION_DB_HOST"
-echo "Nom de base de données : $FUSION_DB_NAME"
-echo "Utilisateur BDD : $FUSION_DB_USER"
+echo -e "${GREEN}PHP Version: $FUSION_PHP_VERSION${RESET}"
+echo -e "${GREEN}FusionResolveIT Version: $FUSION_VERSION ($FUSION_DOWNLOAD_URL)${RESET}"
+echo -e "${GREEN}Hôte BDD : $FUSION_DB_HOST${RESET}"
+echo -e "${GREEN}Nom de base de données : $FUSION_DB_NAME${RESET}"
+echo -e "${GREEN}Utilisateur BDD : $FUSION_DB_USER${RESET}"
 echo " "
 PS3="Est-ce bon ? (Si NON, le script sera relancé à zéro) "
 options=("Oui" "Non")
@@ -195,7 +200,7 @@ select choix in "${options[@]}"; do
             break
             ;;
         *)
-            echo "Version invalide"
+            echo -e "${RED}Version invalide${RESET}"
             ;;
     esac
 done
